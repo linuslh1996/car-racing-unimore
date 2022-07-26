1e0ac9c4d6f877faaaa1d2beeac9479eb6fd554d
Linus Heinzl <linus.heinzl@student.hpi.de>
2022-07-26 23:49:07 +0200

    Add Data

---
+++ train_qlearning.py
@@ -0,0 +1,32 @@
+import sys
+from dataclasses import asdict
+from pathlib import Path
+from typing import List
+
+from src.car_racing import CustomRacing
+from src.q_learning import TrainingParameters, learn_q_values, QNetwork, create_graph_data, QLearningMetadata
+from pandas import DataFrame
+
+weights_path: Path = Path() / "data" / "results"
+metadata_path: Path = Path() / "data" / "metadata"
+weights_folder: Path = weights_path / sys.argv[1]
+weights_folder.mkdir(parents=True, exist_ok=True)
+network = QNetwork(weights_folder)
+assert sys.argv[2] == "-t" or sys.argv[2] == "-i" or sys.argv[2] == "-d"
+episode: int = int(sys.argv[3])
+car_racing: CustomRacing = CustomRacing(episode)
+if sys.argv[2] == "-t":
+    if episode > 0:
+        network.load_model(episode)
+    params: TrainingParameters = TrainingParameters(0.001, 50, True)
+    learn_q_values(car_racing, 0.4, network, params)
+elif sys.argv[2] == "-i":
+    network.load_model(episode)
+    params: TrainingParameters = TrainingParameters(0.001, 50, False)
+    learn_q_values(car_racing, 0.0, network, params)
+elif sys.argv[2] == "-d":
+    metadata: List[QLearningMetadata] = create_graph_data(car_racing, network)
+    as_dataframe: DataFrame = DataFrame(data=[asdict(data) for data in metadata])
+    as_dataframe.to_csv(metadata_path / "q_learning.csv")
+
+

58d54765d542996546509c6efdd8138e28805dcc
linuslh1996 <33374886+linuslh1996@users.noreply.github.com>
2023-05-02 19:38:06 +0200

    Update train_ppo.py

--- train_ppo.py
+++ train_ppo.py
@@ -8,7 +8,7 @@ from pandas import DataFrame
 from src.car_racing import CustomRacing
 from src.ppo import PPONetwork, perform_ppo_learning, get_ppo_data, PPOMetadata
 
-
+# Initialize
 weights_path: Path = Path() / "data" / "results"
 weights_folder: Path = weights_path / sys.argv[1]
 weights_folder.mkdir(parents=True, exist_ok=True)
@@ -17,13 +17,19 @@ network = PPONetwork(weights_folder)
 assert sys.argv[2] == "-t" or sys.argv[2] == "-i" or sys.argv[2] == "-d"
 episode: int = int(sys.argv[3])
 car_racing: CustomRacing = CustomRacing(episode)
+    
+# Training Mode
 if sys.argv[2] == "-t":
     if episode > 0:
         network.load_model(episode)
     perform_ppo_learning(car_racing, network, True)
+    
+# Inference Mode
 elif sys.argv[2] == "-i":
     network.load_model(episode)
     perform_ppo_learning(car_racing, network, False)
+    
+# Collect Metadata Mode (for plots)
 elif sys.argv[2] == "-d":
     metadata: List[PPOMetadata] = get_ppo_data(car_racing, network)
     as_dataframe: DataFrame = DataFrame([asdict(data) for data in metadata])

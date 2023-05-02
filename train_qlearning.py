121236ea95caded80e9f9e357a901c85cfd5322b
linuslh1996 <33374886+linuslh1996@users.noreply.github.com>
2023-05-02 19:37:07 +0200

    Update train_qlearning.py

--- train_qlearning.py
+++ train_qlearning.py
@@ -7,6 +7,7 @@ from src.car_racing import CustomRacing
 from src.q_learning import TrainingParameters, learn_q_values, QNetwork, create_graph_data, QLearningMetadata
 from pandas import DataFrame
 
+# Initialize
 weights_path: Path = Path() / "data" / "results"
 metadata_path: Path = Path() / "data" / "metadata"
 weights_folder: Path = weights_path / sys.argv[1]
@@ -15,15 +16,21 @@ network = QNetwork(weights_folder)
 assert sys.argv[2] == "-t" or sys.argv[2] == "-i" or sys.argv[2] == "-d"
 episode: int = int(sys.argv[3])
 car_racing: CustomRacing = CustomRacing(episode)
+    
+# Training Mode
 if sys.argv[2] == "-t":
     if episode > 0:
         network.load_model(episode)
     params: TrainingParameters = TrainingParameters(0.001, 50, True)
     learn_q_values(car_racing, 0.4, network, params)
+    
+# Inference Mode
 elif sys.argv[2] == "-i":
     network.load_model(episode)
     params: TrainingParameters = TrainingParameters(0.001, 50, False)
     learn_q_values(car_racing, 0.0, network, params)
+    
+# Collect Metadata Mode (for plots)
 elif sys.argv[2] == "-d":
     metadata: List[QLearningMetadata] = create_graph_data(car_racing, network)
     as_dataframe: DataFrame = DataFrame(data=[asdict(data) for data in metadata])
